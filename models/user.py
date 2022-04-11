06d88ab9b2c3a5090817f835d73a9c624f6e965a
FJDeepPyMoreno <paco.jalcama@gmail.com>
2022-04-11 17:50:06 +0200

    Deleting comments from models.users.py

--- models/user.py
+++ models/user.py
@@ -19,35 +19,11 @@ class UserModel(db.Model):
     def find_by_username(cls, username):
         return cls.query.filter_by(username=username).first()  # the 'username' on the left side is the table's variable.
                                                                # the 'username' on the right is the method argument 'username'    
-        # connection = sqlite3.connect('data.db')
-        # cursor     = connection.cursor()
-        # find_query = "SELECT * FROM users WHERE username=?"
-        # susers     = cursor.execute(find_query, (username,))  
-        # row        = susers.fetchone()  
-                                
-        # if row:
-        #     user = cls(*row)   
-        # else:
-        #     user = None
-
-        # connection.close()
-        # return user
 
     @classmethod
     def find_by_id(cls, idu):
         return cls.query.filter_by(id=idu).first()     
-        # connection = sqlite3.connect('data.db')
-        # cursor = connection.cursor()
-        # find_query = "SELECT * FROM users WHERE id=?"
-        # susers = cursor.execute(find_query, (idu,))  
-        # row = susers.fetchone()                    
-        # if row:
-        #     user = cls(*row)  
-        # else:
-        #     user = None
-        # connection.close()
-        # return user
-    
+            
     # Añadimos ahora los siguientes métodos tras la implementación con SQL Alchemy:
     
     def save_to_db(self):
